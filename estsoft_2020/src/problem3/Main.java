593757fa9b5e9d89d9eb0cd1c304b95d3c31cfed
jinsumin <suminjin@naver.com>
2020-03-29 18:12:38 +0900

    -

--- estsoft_2020/src/problem3/Main.java
+++ estsoft_2020/src/problem3/Main.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args) {
-        int[] A = {2, 4, 4, 2};
+        int[] A = {3, 2, 1, 4};
         Solution solution = new Solution();
         System.out.println(solution.solution(A));
     }
